f7d540592fc6d6bef43308dea7700c95ba87ce39
JeissonRuiz0 <jeisonruiz.developer@gmail.com>
2024-01-10 14:21:26 -0500

    Apdate Desing

--- todolist/src/components/ToDoList.jsx
+++ todolist/src/components/ToDoList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import initialData from '../Data.json'; // Asumiendo que los datos iniciales están aquí
 import ToDoItem from "./ToDoItem";
 import './Styles/TodoList.scss';
+import imgBackground from '../assets/background1.png';
 
 const ToDoList = () => {
     const [todos, setTodos] = useState(initialData); // Estado para los ToDos
@@ -31,34 +32,42 @@ const ToDoList = () => {
 
     return (
         <div className="TodoList-container">
-           
+            <div className="TodoList-img">
+                <img src={imgBackground}/>
+            </div>
+            <div className="TodoList-List">
+                <h1>ToDo List</h1>
                 <form onSubmit={handleSubmit}>
-                <div className="TodoList__addTodo">
-                    <input
-                        id="addToDo large-input"
-                        type="text"
-                        className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)} // Actualiza el estado del texto
-                    />
-                    <button type="submit" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Agregar</button>
+                    <div className="TodoList__addTodo">
+                        <input
+                            id="addToDo large-input"
+                            type="text"
+                            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)} // Actualiza el estado del texto
+                        />
+                        <button type="submit" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Agregar</button>
                     </div>
                 </form>
-           
-            <div className="TodoList__Items">
-                {
-                    todos.map(todo => (
-                        <ToDoItem
-                            key={todo.id}
-                            text={todo.text}
-                            completed={todo.completed}
-                            toggleComplete={() => toggleComplete(todo.id)}
-                            deleteToDo={() => deleteToDo(todo.id)}
-                        />
-                    ))
-                }
-            </div>
 
+                <div className="TodoList__Items">
+                    {
+                        todos.map(todo => (
+                            <ToDoItem
+                                key={todo.id}
+                                text={todo.text}
+                                completed={todo.completed}
+                                toggleComplete={() => toggleComplete(todo.id)}
+                                deleteToDo={() => deleteToDo(todo.id)}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+            
+            <div className="TodoList-User">
+                    <h1>hola</h1>
+            </div>
 
 
         </div>
